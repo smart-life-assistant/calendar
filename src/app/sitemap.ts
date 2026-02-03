05d9c84e4dd98430b0d01c0918302d71d521d76c
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2026-02-03 11:17:00 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,10 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "https://calendar-eight-beige.vercel.app";
 
   // Static routes - Important pages for sitelinks
   const routes = [
